68728d3a5fc46dd37bf5f126b1e5ae0174e1aeed
Andrew McClary <andrew.mccalry00@gmail.com>
2018-02-20 16:30:21 -0500

    Finished adding a task, listing all of the incomplete tasks and got the programing set up for removing a task

--- src/com/company/Manager.java
+++ src/com/company/Manager.java
@@ -1,12 +1,52 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class Manager {
+    private final Menu menu;
     ArrayList<Task> completed = new ArrayList<Task>();
     ArrayList<Task> incomplete = new ArrayList<Task>();
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH/mm/ss");
 
-    protected void addTask() {
 
+    public Manager(Menu menu) {
+        this.menu = menu;
+    }
+
+    protected void addTask(Task task) {
+        incomplete.add(task);
+        menu.mainMenu();
+
+    }
+    protected void removeTask(int index){
+        index--;
+        incomplete.remove(index);
+        System.out.println("This task has been successful removed.");
+        menu.mainMenu();
+    }
+    protected void listAll(){
+        System.out.println("All of your current tasks are: ");
+        for(int i = 0; i < incomplete.size(); i++){
+            System.out.println((i + 1) + ". " + incomplete.get(i));
+        }
+        for(int x = 0; x < completed.size(); x++){
+            System.out.println((incomplete.size() + x + 1) + ". " + completed.get(x));
+        }
+        menu.mainMenu();
+    }
+    protected void listIncomplete(){
+        System.out.println("Your incomplete tasks are: ");
+        for(int i = 0; i < incomplete.size(); i++){
+            System.out.println((i + 1) + ". " + incomplete.get(i));
+        }
+        menu.mainMenu();
+    }
+    protected void listCompleted(){
+        System.out.println("Your completed tasks are: ");
+        for(int i = 0; i < completed.size(); i++){
+            System.out.println((i + 1) + ". " + completed.get(i));
+        }
+        menu.mainMenu();
     }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -5,7 +5,74 @@ import java.util.Scanner;
 public class Menu {
 
     Scanner input = new Scanner(System.in);
-    Manager manager = new Manager();
+    Manager manager = new Manager(this);
+    Task task = new Task("", "", "");
 
-
+    protected void mainMenu(){
+        System.out.println("What would you like to do?\n" +
+                "1. Add a Task.\n" +
+                "2. Remove a Task.\n" +
+                "3. See all Tasks.\n" +
+                "4. See incomplete Tasks.\n" +
+                "5. See completed Tasks.\n" +
+                "6. Mark a Task as complete.\n" +
+                "7. Edit a Task.\n" +
+                "8. Select a Task and see the Details.\n" +
+                "9. Exit.");
+        try{
+            switch(input.nextInt()){
+                case 1:
+                    //add
+                    input.nextLine();
+                    System.out.println("What task would you like to add?");
+                    input.nextLine();
+                    System.out.println("When does it need to be finished by?");
+                    input.nextLine();
+                    System.out.println("What needs to be done in this task?");
+                    input.nextLine();
+                    manager.addTask(task);
+                    break;
+                case 2:
+                    //remove
+                    input.nextLine();
+                    System.out.println("What task would you like to remove?");
+                    manager.listIncomplete();
+                    input.nextInt();
+                    manager.removeTask(input.nextInt());
+                    break;
+                case 3:
+                    //List all
+                    manager.listAll();
+                case 4:
+                    //list incomplete
+                    manager.listIncomplete();
+                    break;
+                case 5:
+                    //list completed
+                    manager.listCompleted();
+                    break;
+                case 6:
+                    //mark a task complete
+                    break;
+                case 7:
+                    //Edit
+                    break;
+                case 8:
+                    //View details
+                    break;
+                case 9:
+                    //exit
+                    System.exit(12321);
+                    break;
+                default:
+                    input.nextLine();
+                    System.out.println("Please enter one of the given options.");
+                    mainMenu();
+            }
+        }catch (Exception e){
+            input.nextLine();
+            System.out.println("Please enter one of the given options.");
+            mainMenu();
+        }
+    }
 }
